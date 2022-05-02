7404ea5048892265a57e820d8792cb5030dd1ea2
Samira Kian <kia.sami8@gmail.com>
2022-05-02 11:27:45 +0430

    adding new assignment of javascript

--- page1.js
+++ page1.js
@@ -1,3 +1,11 @@
+let name = prompt('What is your name?');
+let age = prompt('How old are you?');
+let sex = prompt('what is your gender?');
 
-let mymessage = prompt ("What is your last name");
-alert ("Hello"+" "+  mymessage);
+alert(`${name} ${age} ${sex}`)
+
+let num1 = prompt('please inseart a number');
+let num2 = prompt('please choose another number');
+
+alert(`${num1} ${num2}`)
+alert(`${num2} ${num1}`)
